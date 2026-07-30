bf587bdd21dd7aa6424e9ee68719f4493b7baf48
Rohit <155058747+nixen-rohit@users.noreply.github.com>
2026-07-30 22:26:48 +0530

    Time: 10 ms (17.58%) | Memory: 31.1 MB (24.46%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,72 +1,12 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-
-        vector<int> pos, neg;
-
-        for (int x : nums) {
-            if (x < 0) {
-                neg.push_back(x);
-
-            } else {
-                pos.push_back(x);
-            }
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++){
+            nums[i]=nums[i]*nums[i];
+            ans.push_back(nums[i]);
         }
-
-        if (neg.empty()) {
-            for (int &x : pos)
-                x *= x;
-
-            return pos;
-        }
-
-        if (pos.empty()) {
-            for (int &x : neg)
-                x *= x;
-            reverse(neg.begin(), neg.end());
-            return neg;
-        }
-
-        for (int &x : pos) x *= x;
-
-        for (int &x : neg) x *= x;
-      reverse(neg.begin(), neg.end());
-
-        int i = 0 , j = 0 , idx =0;
-
-        int n = neg.size();
-        int m = pos.size();
-        vector <int> res(n+m);
-
-        while(i<n && j<m){
-            if(neg[i]<=pos[j]){
-                res[idx]=neg[i];
-                i++;
-                idx++;
-            }
-            else{
-                res[idx]=pos[j];
-                idx++;
-                j++;
-            }
-        }
-
-        while(i<n){
-            res[idx]=neg[i];
-            idx++;
-            i++;
-        }
-
-        while(j<m){
-            res[idx]=pos[j];
-            idx++;
-            j++;
-        }
-
-        return res;
-
-
-
-       
+       sort(ans.begin(), ans.end());
+       return ans;
     }
 };
