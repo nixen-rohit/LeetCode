381e819eae153b94037e810580848e1f90057800
Rohit <155058747+nixen-rohit@users.noreply.github.com>
2026-08-01 00:27:56 +0530

    Time: 36 ms (97.29%) | Memory: 29.1 MB (73.60%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,44 +1,52 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
 
         sort(nums.begin(), nums.end());
 
         int n = nums.size();
 
-        vector<vector<int>> res;
-
         for (int i = 0; i < n - 2; i++) {
 
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
             int left = i + 1;
-
             int right = n - 1;
-
             int sum = -1 * nums[i];
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
             while (left < right) {
-                int s = nums[left] + nums[right];
 
-                if (s == sum) {
+                int idxsum = nums[left] + nums[right];
+
+                if (sum == idxsum) {
+
                     res.push_back({nums[i], nums[left], nums[right]});
+
                     left++;
                     right--;
 
-                    while (left < right && nums[left] == nums[left - 1])
+                    while (left < right && nums[left] == nums[left - 1]) {
+
                         left++;
-                    while (left < right && nums[right] == nums[right + 1])
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+
                         right--;
-                } else if (s < sum) {
+                    }
+
+                }
+
+                else if (sum > idxsum) {
+
                     left++;
+
                 } else {
                     right--;
                 }
             }
         }
-
         return res;
     }
 };
