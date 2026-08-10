70af328af1acce20053825c7dd36dfc33bee441e
Rohit Chauhan <155058747+nixen-rohit@users.noreply.github.com>
2026-08-10 23:11:58 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (80.77%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
 
-        // brute force
+        // normal approach
 
         // vector<int> even;
         // vector<int> odd;
         // vector<int> res;
-        // int n = nums.size();
 
         // for (int x : nums) {
         //     if (x % 2 == 0) {
@@ -20,24 +19,26 @@ public:
         // for (int x : even) {
         //     res.push_back(x);
         // }
+
         // for (int x : odd) {
         //     res.push_back(x);
         // }
         // return res;
 
-        // optimise
-
-        int i = 0;
         int n = nums.size();
         int j = n - 1;
+        int i = 0;
 
         while (i < j) {
+
             if (nums[i] % 2 == 0) {
                 i++;
             } else if (nums[j] % 2 == 1) {
                 j--;
             } else {
+
                 swap(nums[i], nums[j]);
+
                 i++;
                 j--;
             }
