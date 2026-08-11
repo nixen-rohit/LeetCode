0b40649b766fb1769f6fc67ed706ace9938c9a2a
Rohit Chauhan <155058747+nixen-rohit@users.noreply.github.com>
2026-08-11 23:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (99.87%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-
+        int n = nums.size();
         // brute force
 
-        vector<int> even;
-        vector<int> odd;
-        vector<int> res;
-        int n = nums.size();
+        // vector<int> even;
+        // vector<int> odd;
+        // vector<int> res;
+        
 
-        for (int x : nums) {
-            if (x % 2 == 0) {
-                even.push_back(x);
-            } else {
-                odd.push_back(x);
+        // for (int x : nums) {
+        //     if (x % 2 == 0) {
+        //         even.push_back(x);
+        //     } else {
+        //         odd.push_back(x);
+        //     }
+        // }
+
+        // for (int x : even) {
+        //     res.push_back(x);
+        // }
+        // for (int x : odd) {
+        //     res.push_back(x);
+        // }
+
+
+        //optimised
+
+        int i = 0;
+        int j = n-1;
+
+        while(i<j){
+            if(nums[i]%2==0){
+                i++;
+            }
+            else if(nums[j]%2==1){
+                j--;
+
+            }
+            else{
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
             }
         }
 
-        for (int x : even) {
-            res.push_back(x);
-        }
-        for(int x : odd){
-            res.push_back(x);
-        }
-
-
-        return res;
+        return nums;
     }
 };
